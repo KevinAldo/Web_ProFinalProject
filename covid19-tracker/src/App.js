9a10b12e9af7d785faa395e60c50fecc8f003375
KevinAldo <s21810162@student.unklab.ac.id>
2020-11-28 15:00:37 +0800

    1stCommit

---
+++ covid19-tracker/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './component/header';
+import Hello from './component/hello';
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ covid19-tracker/src/component/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+const Header = () =>{
+    return (
+      <div className="header-container">  
+        <h1>Covid19 Tracker</h1>
+        <h2>Web Programming Smester 1 th. 2020/2021</h2>
+      </div>
+    );
+  };
+
+export default Header;
